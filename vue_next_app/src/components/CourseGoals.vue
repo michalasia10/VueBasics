fc8482d153dfe440770993411dbb53f59a87146f
michalasia10 <misiek1027@o2.pl>
2021-12-24 20:59:06 +0100

    next bigger app in vue

---
+++ vue_next_app/src/components/CourseGoals.vue
@@ -0,0 +1,22 @@
+<template>
+    <ul>
+        <li v-for="goal in goals" :key="goal">
+            <slot :item="goal"></slot>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "CourseGoals",
+        data() {
+            return {
+                goals: ['Finish the course', 'Learn a Vue']
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
