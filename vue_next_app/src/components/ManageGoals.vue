f2c0cb1410849b7f11ca77d4c76433089d045c90
michalasia10 <misiek1027@o2.pl>
2021-12-24 21:51:35 +0100

    apply teleport and error component

--- vue_next_app/src/components/ManageGoals.vue
+++ vue_next_app/src/components/ManageGoals.vue
@@ -1,13 +1,39 @@
 <template>
     <div>
         <h2> Manage </h2>
-        <input type="text">
+        <input type="text" ref="goal">
+        <button @click="setGoal">Set Goal</button>
+        <teleport to="body">
+            <error-alert v-if="inputIsValid">
+            <h2>Input is invalid!</h2>
+            <p>Please enter at least a few characters...</p>
+            <button @click="confirmError">Confirmed</button>
+        </error-alert>
+        </teleport>
     </div>
 </template>
 
 <script>
+    import ErrorAlert from "./ErrorAlert";
     export default {
-        name: "ManageGoals"
+        name: "ManageGoals",
+        components: {ErrorAlert},
+        data(){
+            return{
+                inputIsValid:false,
+            }
+        },
+        methods:{
+            setGoal(){
+                const enternedValue = this.$refs.goal.value;
+                if (enternedValue === ''){
+                    this.inputIsValid = true
+                }
+            },
+            confirmError(){
+              this.inputIsValid = false
+            }
+        }
     }
 </script>
 
